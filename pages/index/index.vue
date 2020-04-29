eb3091d7b157b3d795ff60bb45dd922ad334181f
batian123 <18317838895@163.com>
2020-04-29 17:26:12 +0800

    test1

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -5,6 +5,16 @@
 				<swiper-item v-for="(item, index) in bannerList" :key="index"><image :src="item.bannerImg"></image></swiper-item>
 			</swiper>
 		</view>
+		<view class="blogList">
+			<view class="blogCount" v-for="item in blogData" :key="item._id">
+				<view class="blogTilte">
+					{{item.title}}
+				</view>
+				<view class="blogText">
+					{{item.content | filterDiv}}
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -14,27 +24,20 @@
 			return {
 				title: 'Hello',
 				bannerList:[],
+				blogData:[],
 			}
 		},
 		onLoad() {
 			this.getBanner()
+			this.getBlogList();
+		},
+		filters:{
+			filterDiv:(data)=>{
+				return 'aa'
+			}
 		},
 		methods: {
 			async getBanner(){
-				// uni.request({
-				// 	url:"http://localhost:8633/api/web/queryAllBannerIsTrue",
-				// 	success:(resp)=> {
-				// 		console.log(resp)
-				// 		if(resp.data.status==1){
-				// 			uni.showToast({
-				// 			    title: `${resp.data.massage}`,
-				// 			    duration: 2000
-				// 			});
-				// 			this.bannerList = resp.data.rows
-				// 		}
-				// 	},
-				// 	method:"GET"
-				// })
 				const bannerData = await this.$myApi({
 					url:'api/web/queryAllBannerIsTrue'
 				})
@@ -46,6 +49,20 @@
 					});
 					this.bannerList = bannerData.data.rows
 					console.log(this.bannerList)
+				}
+			},
+			async getBlogList(){
+				const data =await this.$myApi({
+					url:'api/web/queryPageBlog?pageSize=6&pageIndex=1&tags='
+				})
+				console.log(data)
+				if(data.data.status==1){
+					this.blogData = data.data.bloglist
+				}else{
+					uni.showToast({
+						title: `${bannerData.data.massage}`,
+						duration: 2000
+					});
 				}
 			}
 		}
@@ -64,6 +81,12 @@
 	.swCount{ 
 		swiper{width: 750rpx;height: 380rpx;}
 		image{width:100%;height: 380rpx;}
+	}
+	.blogList{
+		.blogCount{
+			.blogTilte{}
+			.blogText{}
+		}
 	}
 	
 </style>
